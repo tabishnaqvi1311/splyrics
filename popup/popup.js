66564a22e8f5134ffde9800f4eab3e3ebe3bec3e
Tabish Naqvi <tabish.naqvi2003@gmail.com>
2024-02-20 09:50:02 +0530

    initiated project

---
+++ popup/popup.js
@@ -0,0 +1,1 @@
+console.log("me is popup")
---
+++ scripts/content.js
@@ -0,0 +1,15 @@
+const spotifyUrl = "https://open.spotify.com"
+
+chrome.runtime.onInstalled.addListener(async(tab) => {
+    if(tab.url.startsWith(spotifyUrl)){
+        const prevState = await chrome.action.getBadgeText({tabId: tab.id});
+        const nextState = prevState === "ON" ? "OFF" : "ON"
+
+        await chrome.action.setBadgeText({
+            tabId: tab.id,
+            text: nextState
+        })
+
+        console.log(tab.id);
+    }
+})
