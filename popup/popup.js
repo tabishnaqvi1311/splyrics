eb3693d86f9e1fd3dd601744a4405adb609ce0cb
Tabish Naqvi <tabish.naqvi2003@gmail.com>
2024-02-23 22:28:54 +0530

    popup added and manifest updated

--- popup/popup.js
+++ popup/popup.js
@@ -1,1 +1,17 @@
-console.log("me is popup")
+document.addEventListener("DOMContentLoaded", () => {
+    chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
+        let url = tabs[0].url;
+        let isSpotify = url.includes("open.spotify.com");
+        let status = document.getElementById('status')
+
+        if (isSpotify) {
+            status.innerText = "Waiting For Lyrics..."
+            status.style.color = "#e0ca00"
+            return;
+        }
+        else {
+            status.innerText = "Not On Spotify"
+            status.style.color = "red"
+        }
+    })
+})
